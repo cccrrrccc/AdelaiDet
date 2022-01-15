e399974d3cfe4459da1a52b2a7661942d8a6c874
Ruichen Chen <ruichen1@ualberta.ca>
2022-01-15 16:42:42 -0700

    Update segment_bed.py

--- tools/segment_bed.py
+++ tools/segment_bed.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     # Read the image
     img = cv2.imread('/home/ruichen/AdelaiDet/test_images/content000.jpg')
     with open('/home/ruichen/AdelaiDet/test_images_seg_res/img_prediction_mask.pkl', 'rb') as handle:
-        mask = pickle.load(handle)[0].tolist()
+        mask = pickle.load(handle)[0]
     print(len(mask))
     img_bg = cv2.bitwise_and(img, img, mask = mask)
     cv2.imwrite('test.jpg', img_bg)
