0203378f074dbad91330fd594068fb5bc4a3cea8
Ruichen Chen <ruichen1@ualberta.ca>
2022-01-16 22:37:00 -0700

    Create combine_bed.py

---
+++ tools/combine_bed.py
@@ -0,0 +1,18 @@
+import cv2
+import pickle
+
+if __name__ == "__main__":
+    # Read the image
+    img1 = cv2.imread('/home/ruichen/AdelaiDet/test_images/content000.jpg')
+    img2 = cv2.imread('/home/ruichen/AdelaiDet/test_images/0_0.png')
+    with open('/home/ruichen/AdelaiDet/test_images_seg_res/img_prediction_mask.pkl', 'rb') as handle:
+        mask = pickle.load(handle)[0].tolist()
+
+    for i in range(0, len(mask)):
+        for j in range(0, len(mask[0])):
+            if mask[i][j] == False:
+                img1[i][j] = img2[i][j]
+
+
+    #img_bg = cv2.bitwise_and(img, img, mask = mask)
+    cv2.imwrite('combine.jpg', img1)
